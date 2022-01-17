abe2c8eed75675d6a04e199d3109fe4cafdf06b6
ehdgnsdl <ehdgnsdl9192@gmail.com>
2022-01-17 23:30:09 +0900

    Create 20220117_그리디_만수금.py

---
+++ 2022_programmers/20220117_그리디_만수금.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = list(map(int, input().split()))
+b.sort() # 1 1 2 3 9
+
+target = 1
+for x in b:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    if target < x:
+        break
+    target += x
+    
+
+print(target)
+
