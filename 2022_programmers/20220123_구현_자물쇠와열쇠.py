c458209c5c1a4ad5f4b9c17bb337ab6961adbd58
ehdgnsdl <ehdgnsdl9192@gmail.com>
2022-01-24 12:00:47 +0900

    20220124

---
+++ 2022_programmers/20220123_구현_자물쇠와열쇠.py
@@ -0,0 +1,54 @@
+# 자물쇠와 열쇠 (완전 탐색 이용)
+# rotate_a_matrix_by_90_degree(): 2차원 리스트를 90도 회전한 결과를 반환하는 함수
+# https://programmers.co.kr/learn/courses/30/lessons/60059
+# 2차원 리스트 90도 회전
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a)  # 행 길이 계산
+    m = len(a[0])  # 열 길이 계산
+    result = [[0] * n for _ in range(m)]  # 결과 리스트
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
+
+
+# 자물쇠의 중간 부분이 모두 1인지 확인
+def check(new_lock):
+    lock_length = len(new_lock) // 3
+    for i in range(lock_length, lock_length * 2):
+        for j in range(lock_length, lock_length * 2):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+
+
+def solution(key, lock):
+    n = len(lock)
+    m = len(key)
+    # 자물쇠의 크기를 기존의 3배로 변환
+    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
+    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
+    for i in range(n):
+        for j in range(n):
+            new_lock[i + n][j + n] = lock[i][j]
+
+    # 4가지 방향에 대해서 확인
+    for rotation in range(4):
+        key = rotate_a_matrix_by_90_degree(key)  # 열쇠 확인
+        # n*2를 해주는 이유는. 전체가 9 X 9가 되었고 lock이 3 x 3 이므로
+        # x, y좌표는 이동좌표이다. 3에서 9까지가려면 6이 필요하므로 n*2를 해준 것으로 보인다.
+        for x in range(n * 2):
+            for y in range(n * 2):
+                # 자물쇠에 열쇠를 끼워 넣기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] += key[i][j]
+                # 새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
+                if check(new_lock) == True:
+                    return True
+                # 자물쇠에서 열쇠를 다시 빼기
+                for i in range(m):
+                    for j in range(m):
+                        new_lock[x + i][y + j] -= key[i][j]
+
+    return False
